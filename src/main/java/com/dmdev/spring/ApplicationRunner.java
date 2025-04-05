823888fff3353d1aaf77192850a33447da09e29a
tmikhaylovskaya <tmikhaylovskaya1981@gmail.com>
2025-04-05 17:27:58 +0300

    обновление зависимостей

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -11,10 +11,10 @@ import java.io.Serializable;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        String value = "hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+//        String value = "hello";
+//        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+//        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+//        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
 
         try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
     //      clazz -> String -> Map<String, Object>
