79f38cbd4368c69a2083662505f7547a7892b63c
tmikhaylovskaya <tmikhaylovskaya1981@gmail.com>
2025-04-08 15:36:26 +0300

    обновление зависимостей

--- src/main/java/com/dmdev/spring/listener/entity/EntityListener.java
+++ src/main/java/com/dmdev/spring/listener/entity/EntityListener.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EntityListener {
 
+//    @EventListener(condition = "#root.args[0].accessType.name() == 'READ'")
     @EventListener(condition = "#p0.accessType.name() == 'READ'")
     @Order(10)
     public void acceptEntityRead(EntityEvent entityEvent) {
